3ad6e0df6a4c921cc2c1d9b57b176f9733523152
jatinsajwan3841 <jatinsajwan3841@gmail.com>
2023-02-06 15:07:09 +0530

    first working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [studData, setStudData] = React.useState([]);
+    const [forceRender, setForceRender] = React.useState(0);
+
+    const getData = async () => {
+        let dat = await fetch(
+            "https://63e0bacd59bb472a74278f0f.mockapi.io/api/v1/students"
+        );
+        dat = await dat.json();
+        setStudData(dat);
+    };
+
+    const sortData = () => {
+        let sortedDat = studData.sort((a, b) => a.class - b.class);
+        setStudData(sortedDat);
+        setForceRender((prev) => prev + 1);
+    };
+
+    React.useEffect(() => {
+        getData();
+    }, []);
+    return (
+        <div className="val-table">
+            <button onClick={sortData}>Sort by Class</button>
+            <h2>Fake Student Data</h2>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Avatar</th>
+                        <th>Name</th>
+                        <th>Class</th>
+                        <th>Marks</th>
+                        <th>Phone</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {studData &&
+                        studData.map((data, i) => (
+                            <tr key={i}>
+                                <td>
+                                    <img src={data.avatar} className="avatar" />
+                                </td>
+                                <td>{data.name}</td>
+                                <td>{data.class}</td>
+                                <td>{data.marks}</td>
+                                <td>{data.phone}</td>
+                            </tr>
+                        ))}
+                </tbody>
+            </table>
+        </div>
+    );
+};
 
 export default App;
